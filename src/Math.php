581e7b10ed363974b9ef2c924228a1bfbdf81e25
Eliecer Garcia Rey <erey@freepik.com>
2023-07-07 17:17:06 +0200

    feat: first commit

---
+++ src/Math.php
@@ -0,0 +1,24 @@
+<?php
+
+class Math
+{
+    public function add(int $x, int $y): int
+    {
+        return $x + $y;
+    }
+
+    public function sub(int $x, int $y): int
+    {
+        return $x - $y;
+    }
+
+    public function mul(int $x, int $y): int
+    {
+        return $x * $y;
+    }
+
+    public function div(int $x, int $y): int
+    {
+        return $x / $y;
+    }
+}
